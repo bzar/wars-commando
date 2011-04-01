005b21bc89e7ed5e0dc5eb7e3e1ea52ba600a6c3
teemu.erkkola@gmail.com <teemu.erkkola@gmail.com@6f2bd846-2eeb-1103-b2bd-52203d9cafa9>
2011-04-01 15:22:29 +0000

    fixed bug in enemy infantry falling prevention algorithm

--- trunk/enemyinfantry.cpp
+++ trunk/enemyinfantry.cpp
@@ -1,17 +1,17 @@
 /*
  * Wars: Commando
  * Copyright (C) 2011 Teemu Erkkola
- * 
+ *
  * This program is free software; you can redistribute it and/or
  * modify it under the terms of the GNU General Public License
  * as published by the Free Software Foundation; either version 2
  * of the License, or (at your option) any later version.
- * 
+ *
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
- * 
+ *
  * You should have received a copy of the GNU General Public License
  * along with this program; if not, write to the Free Software
  * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
@@ -121,7 +121,7 @@ void EnemyInfantry::preUpdate()
         if(dontFall && onGround)
         {
             QPointF checkPosition = pos() + QPointF((velocity.x() < 0 ? 2 : 4) * pixmap().width()/5, pixmap().height() - 1) + velocity;
-            checkPosition.setY(checkPosition.y() + pixmap().height()/4);
+            checkPosition.setY(checkPosition.y() + pixmap().height()/2);
             AbstractBlock* block = game->getLevel()->getBlockAt(checkPosition);
             if(!block || block->hurts())
             {
